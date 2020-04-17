afcc935b6ca4eb7b225d99e9cccf0ee35a30fde6
Manoel Martins <mano.martins29@gmail.com>
2020-04-17 02:07:20 -0300

    Finish the models

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
+import { getCustomRepository, getRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
     // TODO
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const checkCategoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!checkCategoryExists) {
+      const categoryCreate = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(categoryCreate);
+    }
+
+    const checkCategoryId = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: checkCategoryId?.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
